fba42d81fa62df9a1baf7f74e117970c94545f74
Maz <mazicky@gmail.com>
2016-06-02 00:17:30 +0900

    실패하는 테스트 추가

--- test/test.js
+++ test/test.js
@@ -1,9 +1,25 @@
 import chai from 'chai'
-
+import Door from '../src/door/door'
+import { DOOR_STATE } from '../src/door/types'
 chai.should()
 
+let door = null
+const DOOR_COUNT = 100
+
 describe ('Test', () => {
-  it ('should return the index of given number', () => {
-    [ 1, 2, 3 ].indexOf(1).should.equal(0)
+
+  before(() => {
+    door = new Door()
+  })
+
+  beforeEach(() => {
+    door.init(DOOR_COUNT)
+  })
+
+  it ('should return door state object', () => {
+    door.visitEach(1)
+    door.getState().should.deep.equal(
+      Door.makeDoors(DOOR_COUNT, DOOR_STATE.OPEN)
+    )
   })
 })
