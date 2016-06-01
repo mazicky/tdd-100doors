d906a45ae27602ba2570def969ddec2d8906d160
Maz <mazicky@gmail.com>
2016-06-01 23:05:55 +0900

    Lint 에러 제거

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 import chai from 'chai'
 
-chai.should();
+chai.should()
 
 describe ('Test', () => {
   it ('should return the index of given number', () => {
-    [1, 2, 3].indexOf(1).should.equal(0);
-  });
-});
+    [ 1, 2, 3 ].indexOf(1).should.equal(0)
+  })
+})
